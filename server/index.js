0b9b61f2a39eb2fe5a11706e20b00bbef9bed786
suheda-seyda-sener <t3sesu00@students.oamk.fi>
2024-03-21 14:39:02 +0200

    part 7 completed

--- server/index.js
+++ server/index.js
@@ -3,69 +3,20 @@ require('dotenv').config()
 // Import required express, cors and pg libraries
 const express =require('express')
 const cors = require ('cors')
-//const {Pool} = require('pg')
-const { query } = require('./helpers/db.js')
+const todoRouter  = require('./routes/todo.js')
+
 // Create an Express application
 const app = express()
-
 // Use cors middleware
 app.use(cors())
 // Allow reading posted values from the client as JSON
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use('/', todoRouter)
 
 
 // Instead of using hardcoded value read port from .env file
 const port = process.env.PORT
-
-// Define a route for the root endpoint '/'
-app.get("/",async (req, res) => {
-    // Retrieve data from the database
-    console.log(query)
-    try {
-        const result= await query('select * from task')
-        const rows = result.rows ? result.rows : []
-        res.status(200).json(rows)
-    } catch (error) {
-        console.log(error)
-        res.statusMessage = error
-        res.status(500).json({error: error})
-    }
-})
-
-// Define a route to handle POST requests to create a new task
-app.post("/new",async (req, res) => {
-    try {
-      // Execute SQL INSERT statement to insert a new task into the database
-      const result = await query('insert into task (description) values ($1) returning *',
-      [req.body.description])
-      // If successful, send a success response with status code 200 and the ID of the newly inserted task
-      res.status(200).json({id: result.rows[0].id})
-    } catch (error){
-        console.log(error)
-        res.statusMessage = error
-        // If an error occurs, send an error response with status code 500
-        res.status(500).json({error: error}) 
-    }
-})
-
-// Implement deletion functionality to the backend.  Create delete method, that receives id as query parameter
-app.delete("/delete/:id", async(req,res) => {
-    // Extract the task ID from the request parameters
-    const id = Number(req.params.id)
-    try {
-      // Execute SQL DELETE statement to delete the task by ID
-      const result = await query('delete from task where id = $1',
-      [id])
-      // If successful, send a success response with status code 200 and the deleted task ID
-      res.status(200).json({id:id})
-    } catch (error) {
-        console.log(error)
-        res.statusMessage = error
-        // If an error occurs, send an error response with status code 500
-        res.status(500).json({error: error}) 
-    }
-})
 
 // Set up express app to listen on port
 app.listen(port)
